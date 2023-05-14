a87cef37f282baf33bb55e6385d526c275ebe9d1
redfl0wer <125115138+redfl0wer@users.noreply.github.com>
2023-05-14 16:33:00 +0900

    Put wwr job find code in function and start job find at indeed.com

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "php"
+jobs = extract_wwr_jobs("python")
+print(jobs)
 
-response = get(f"{base_url}{search_term}") 
-
-if response.status_code != 200: 
-  print("can't request website")
-else:
-    results = []
-    soup = BeautifulSoup(response.text, "html.parser") #response.text는 웹사이트의 코드를 줌줌
-    jobs = soup.find_all('section', class_="jobs")
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
-            link = anchor['href']
-            try:
-                company, kind, region = anchor.find_all('span', class_="company")
-            except ValueError:
-                company, kind = anchor.find_all('span', class_="company")
-            title = anchor.find('span', class_='title')
-            job_data = {
-                'company' : company.string,
-                'region' : region.string,
-                'position' : title.string
-                }
-            results.append(job_data)
-    for result in results:
-        print(result)
-        print("")
                 
