bfe4e8b06373a8fc71236e206f729ca584d988b3
redfl0wer <125115138+redfl0wer@users.noreply.github.com>
2023-05-22 13:20:34 +0000

    make job scrapper website with flask & file exporting

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
-keyword = input("What do you want to search for?")
+app = Flask("JobScrapper") #initialize app
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
+db = {}
 
-jobs = indeed + wwr
+@app.route("/") #should be put on top of the func(decorator  )
+def home():
+  return render_template("home.html", name="abc")
 
-file = open(f"Jobs for {keyword}.csv", "w")
-file.write("Position,Company,Location,URL\n")
- 
-for job in jobs:
-  file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+@app.route("/search")
+def search():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    db[keyword] = jobs
+  return render_template("search.html", keyword=keyword, jobs=jobs)
 
-file.close()
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True) #to trigger download
 
-
+app.run("0.0.0.0") #tell we are running a web server here
